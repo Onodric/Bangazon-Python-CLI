e6e5ede2c1d99c492fabd743c8c1394b8d67dbb9
juliakimchung <julhkm@gmail.com>
2017-02-11 10:55:08 -0600

    added docstrings to test_product_popularity.py

--- tests/test_product_popularity.py
+++ tests/test_product_popularity.py
@@ -20,27 +20,45 @@ class TestOrderData(unittest.TestCase):
 
 	@classmethod
 	def setUpClass(self):
+		"""
+		 Create an instance of the product_p that can be used in all tests
+		"""
 		self.product_p = product_cli.ProductPopularity()
 
 	
 
 	def test_can_get_product_data(self):
+		"""
+		Method to test if data from db can be retrieved
+
+		"""
 		self.assertIsNotNone(self.product_p.line_items)
 		self.assertIsNotNone(self.product_p.order_data)
 		self.assertIsNotNone(self.product_p.product_data)
 
 	def test_can_retrieve_total_revenue(self):
+		"""
+		Method to test if get_total_revenue method brings back a dict()
+		"""
 		total_price = self.product_p.get_total_revenue()
 		self.assertIsInsatance(total_price, dict)
 
 
 	def test_can_retrieve_num_of_customers(self):
+		"""
+		Method to test if get_total_num_of_customers method brings back numbers in a dict()
+
+		"""
 		num = self.product_p.get_total_num_of_customers()
-		self.assertIsEqual(num, 10)
+		self.assertIsEqual(num[key], 10)
 
 	def test_can_retrieve_total_num_of_orders(self):
+		"""
+		Method to test if get_total_num_of_orders method brings back numbers in a dict()
+
+		"""
 		num = self.product_p.get_total_num_of_orders()
-		self.assertIsEqual(num, 7)
+		self.assertIsEqual(num[key], 7)
 
 
 
