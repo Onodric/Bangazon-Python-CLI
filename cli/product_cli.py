ed4fd9419d3324d0f873c86563b0e9f2c9d06836
juliakimchung <julhkm@gmail.com>
2017-02-12 21:05:44 -0600

    test for Productpopularity.run()

--- cli/product_cli.py
+++ cli/product_cli.py
@@ -14,57 +14,57 @@ class ProductPopularity():
         Author: Julia Kim-Chung
     """
 
-    def __init__(self):
+    # def __init__(self):
 
-        """
-        """
+    #     """
+    #     """
 
-        self.order_data = Orders.get_all_orders()
-        self.product_data = Product.get_all_products()
-        self.line_items = Line_items.get_all_line_items()
-        self.customer_data = Customer.get_all_customers()
-
-
-    def get_total_revenue(self):
-        revenue = dict()
-        for order in order_data:
-            for item in line_items:
-                if order[0] == item[1]:
-                    for product in product_data:
-                        if product[0] == item[2]:
-                            revenue[product[1]] =list()
-                            revenue[product[1]].append(product[2])
-                            sum(revenue[product[1]])
-
-    return revenue
+    #     self.order_data = Orders.get_all_orders()
+    #     self.product_data = Product.get_all_products()
+    #     self.line_items = Line_items.get_all_line_items()
+    #     self.customer_data = Customer.get_all_customers()
 
 
-    def get_total_num_of_customers(self):
-        customers = dict()
-        for order in order_data:
-            for item in line_items:
-                if order[0] == item[1]:
-                    for product in product_data:
-                        for customer in customer_data:
-                            if product[0] == item[2]:
-                                if order[3] == customer[0]:
-                                    customers[product[1]] ==list()
-                                    customers[product[1]].append(order[3])
-                                    customers[product[1]] == len(cutomers[product[1]])
-    return customers
+    # def get_total_revenue(self):
+    #     revenue = dict()
+    #     for order in order_data:
+    #         for item in line_items:
+    #             if order[0] == item[1]:
+    #                 for product in product_data:
+    #                     if product[0] == item[2]:
+    #                         revenue[product[1]] =list()
+    #                         revenue[product[1]].append(product[2])
+    #                         sum(revenue[product[1]])
+
+    #     return revenue
 
 
-    def get_total_num_of_orders(self):
-        orders = dict()
-        for order in order_data:
-          for item in line_items:
-            if order[0] == line_item[1]:
-                for product in product_data:
-                    if product[0] == item[2]:
-                        orders[product[1]] == list()
-                        orders[product[1]].append(order[0])
-                        orders[product[1]] == len(orders[product[1]])
-    return orders
+    # def get_total_num_of_customers(self):
+    #     customers = dict()
+    #     for order in order_data:
+    #         for item in line_items:
+    #             if order[0] == item[1]:
+    #                 for product in product_data:
+    #                     for customer in customer_data:
+    #                         if product[0] == item[2]:
+    #                             if order[3] == customer[0]:
+    #                                 customers[product[1]] ==list()
+    #                                 customers[product[1]].append(order[3])
+    #                                 customers[product[1]] == len(cutomers[product[1]])
+    #     return customers
+
+
+    # def get_total_num_of_orders(self):
+    #     orders = dict()
+    #     for order in order_data:
+    #       for item in line_items:
+    #         if order[0] == line_item[1]:
+    #             for product in product_data:
+    #                 if product[0] == item[2]:
+    #                     orders[product[1]] == list()
+    #                     orders[product[1]].append(order[0])
+    #                     orders[product[1]] == len(orders[product[1]])
+    #     return orders
 
 
     def run(self):
@@ -73,12 +73,19 @@ class ProductPopularity():
         
         """
 
-        total_revenue = ProductPopularity.get_total_revenue()
-        total_customers = ProductPopularity.get_total_num_of_customers()
-        total_orders = ProductPopularity.get_total_num_of_orders()
+        # total_revenue = ProductPopularity.get_total_revenue()
+        # total_customers = ProductPopularity.get_total_num_of_customers()
+        # total_orders = ProductPopularity.get_total_num_of_orders()
+        print("{}{:^18}{:^11}{:^11}{:^15}".format("Product", "Order", "Customers", "Revenue","") )
+        print ( "{:*^55}".format("*"))
+        
+        print("{}".format("Baby Powder"))
 
 
-    
+    if __name__ == '__main__':
+        product_pop = ProductPopularity()
+        product_pop.run()
+        
 
 
 
