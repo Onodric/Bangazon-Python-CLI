af46d36f0f95deea9bc569d84d1102ab9c7dc3a2
Ben Marks <onodric@yahoo.com>
2017-02-14 09:12:14 -0600

    updated gitignore and removed configuration.py

--- configuration.py
+++
@@ -1,5 +0,0 @@
-import os.path
-
-# filepath = 'c:/Users/marce/bangazon-cli/db/bangazon.db'
-package_dir = os.path.abspath(os.path.dirname(__file__))
-database_path = os.path.join(package_dir, 'db\\bangazon.db')
