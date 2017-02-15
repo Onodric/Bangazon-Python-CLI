89c5536fd3da14ec1a0a3bddbf0296fcc9e0535d
juliakimchung <julhkm@gmail.com>
2017-02-15 08:41:01 -0600

    added tests for all four methods

--- tests/test_product_popularity.py
+++ tests/test_product_popularity.py
@@ -51,9 +51,23 @@ class TestOrderData(unittest.TestCase):
 		and an integer value
 		"""
 		total_price = self.product_p.get_total_revenue()
-		self.assertIsInstance(total_price, dict)
-		for key, val in total_price.items():
-			self.assertEqual(type(total_price[key]), int)
+		price_t = dict()
+		order_num = self.orders_all
+		line_items = self.line_item_all
+		prod_id = self.prod_all
+		for orders in order_num:
+			for line_item in line_items:
+				if orders[0] == line_item[1]:
+					for prod in prod_id:
+						if orders[2] == prod[0]:
+							price_t[prod[0]] = list()
+							price_t[prod[0]].append(prod[1])
+							self.assertIn(prod[1], price_t[prod[0]])
+							self.assertEqual(orders[2], prod[0])
+							self.assertIsInstance(total_price, dict)
+
+							for key, val in total_price.items():
+								self.assertEqual(type(total_price[key]), int)
 
 
 
@@ -63,12 +77,16 @@ class TestOrderData(unittest.TestCase):
 		and an integer value
 		"""
 		num = self.product_p.get_total_num_of_customers()
+		customer_n = dict()
 		customer_id = self.customer_all
 		order_num = self.orders_all
 		line_items = self.line_item_all
 		for orders in order_num:
 			for customer in customer_id:
 				if orders[3] == customer[0]:
+					customer_n[customer[0]] = list()
+					customer_n[customer[0]].append(customer[0])
+					self.assertIn(customer[0], customer_n[customer[0]])
 					self.assertEqual(orders[3], customer[0])
 
 					for key, val in num.items():
@@ -81,6 +99,7 @@ class TestOrderData(unittest.TestCase):
 		and an integer value
 		"""
 		num = self.product_p.get_total_num_of_orders()
+		order_n = dict()
 		order_num = self.orders_all
 		line_items = self.line_item_all
 		prod_id = self.prod_all
@@ -89,10 +108,15 @@ class TestOrderData(unittest.TestCase):
 				if orders[0] == line_item[1]:
 					for prod in prod_id:
 						if orders[2] == prod[0]:
+							order_n[orders[0]] = list()
+							order_n[orders[0]].append(orders[0])
+							self.assertIn(orders[0], order_n[orders[0]])
 							self.assertEqual(orders[2], prod[0])
+
 							for key, val in num.items():
-								self.assertEqual(type(num[key]), int)
+								self.assertEqual(type(num[key]),int)
 								self.assertIsNotNone(num[key])
+
 
 
 
